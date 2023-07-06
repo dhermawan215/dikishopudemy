b89b682fd182cad3d8dbbff38e6f7efe2a28c656
dhermawan215 <dhermawan215@gmail.com>
2023-07-06 23:28:14 +0700

    templating admin

---
+++ resources/views/admin/layouts/header.blade.php
@@ -0,0 +1,22 @@
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--favicon-->
+<link rel="icon" href="{{ asset('themes/Backend-themes/assets/images/favicon-32x32.png') }}" type="image/png" />
+<!--plugins-->
+<link href="{{ asset('themes/Backend-themes/assets/plugins/vectormap/jquery-jvectormap-2.0.2.css') }}" rel="stylesheet" />
+<link href="{{ asset('themes/Backend-themes/assets/plugins/simplebar/css/simplebar.css') }}" rel="stylesheet" />
+<link href="{{ asset('themes/Backend-themes/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}"
+    rel="stylesheet" />
+<link href="{{ asset('themes/Backend-themes/assets/plugins/metismenu/css/metisMenu.min.css') }}" rel="stylesheet" />
+<!-- loader-->
+<link href="{{ asset('themes/Backend-themes/assets/css/pace.min.css') }}" rel="stylesheet" />
+<script src="{{ asset('themes/Backend-themes/assets/js/pace.min.js') }}"></script>
+<!-- Bootstrap CSS -->
+<link href="{{ asset('themes/Backend-themes/assets/css/bootstrap.min.css') }}" rel="stylesheet">
+<link href="{{ asset('themes/Backend-themes/assets/css/app.css') }}" rel="stylesheet">
+<link href="{{ asset('themes/Backend-themes/assets/css/icons.css') }}" rel="stylesheet">
+<!-- Theme Style CSS -->
+<link rel="stylesheet" href="{{ asset('themes/Backend-themes/assets/css/dark-theme.css') }}" />
+<link rel="stylesheet" href="{{ asset('themes/Backend-themes/assets/css/semi-dark.css') }}" />
+<link rel="stylesheet" href="{{ asset('themes/Backend-themes/assets/css/header-colors.css') }}" />
